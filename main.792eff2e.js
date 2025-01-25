8add1e75b80b12b1e925bbc41cbaef8bd80de215
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-25 12:51:59 +0200

    make build

---
+++ main.792eff2e.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"5sFV":[function(require,module,exports) {
+module.exports="Photo.0d321768.png";
+},{}],"eISi":[function(require,module,exports) {
+module.exports="Photo2.74a17766.png";
+},{}],"kn5W":[function(require,module,exports) {
+module.exports="Photo3.3c6b668c.svg";
+},{}],"6KIz":[function(require,module,exports) {
+"use strict";var e=o(require("../images/Photo.png")),t=o(require("../images/Photo2.png")),n=o(require("../images/Photo3.svg"));function o(e){return e&&e.__esModule?e:{default:e}}var r=[e.default,t.default,n.default],u=document.querySelector(".waves"),c=document.querySelector(".waves__button-first"),a=document.querySelector(".waves__button-second"),l=0;function s(){u.style.backgroundImage="url(".concat(r[l],")")}c.addEventListener("click",function(){l=(l-1+r.length)%r.length,s(),console.log(addEventListener)}),a.addEventListener("click",function(){l=(l+1)%r.length,s(),s(),console.log(addEventListener)}),s();
+},{"../images/Photo.png":"5sFV","../images/Photo2.png":"eISi","../images/Photo3.svg":"kn5W"}]},{},["6KIz"], null)
+//# sourceMappingURL=main.792eff2e.js.map
